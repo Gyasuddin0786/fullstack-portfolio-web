8d03094c398ae9989de873038650f569faa05a89
Gyasuddin <gyasuddinansari7854@gmail.com>
2026-07-16 18:50:36 +0530

    Github icons changes

--- client/src/components/ui/ProjectCard.jsx
+++ client/src/components/ui/ProjectCard.jsx
@@ -1,5 +1,5 @@
 import { ExternalLink, Eye } from 'lucide-react';
-import { FaGithub} from "react-icons/fa6";
+import { FaGithub} from "react-icons/fa";
 
 import { Link } from 'react-router-dom';
 
